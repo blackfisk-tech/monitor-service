eeaa7a54523f0a9b9fcfaaefbee03a02807adada
Jeremy R DeYoung <Jeremy@NextStepGuru.com>
2018-01-21 16:38:40 -0600

    type-o's to if []

--- install.sh
+++ install.sh
@@ -3,15 +3,15 @@
 # ### ~/install.sh "PI" "$DEVICEID" "CPE"
 #
 SERVERTYPE="MS"
-if [ "$1" != ""]; then
+if [ "$1" != "" ]; then
   SERVERTYPE="$1"
 fi
 DEVICEID=$(sudo dmidecode -t 4 | grep ID | sed 's/.*ID://;s/ //g' | md5sum | awk '{print $1}')
-if [ "$2" != ""]; then
+if [ "$2" != "" ]; then
   DEVICEID="$1"
 fi
 DATACENTER="CPE"
-if [ "$3" != ""]; then
+if [ "$3" != "" ]; then
   DATACENTER="$1"
 fi
 
