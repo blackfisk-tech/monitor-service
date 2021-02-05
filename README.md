d193402e85dfd4829b6ebb5763d4dd452fa38cdb
Garion Berg <garionberg@gmail.com>
2021-02-05 08:09:59 +0000

    Support communication with >1 socket.io servers

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # monitor-service
+#### Install Arguments: In this example, the first argument "PI" is the server type abbreviation, the second argument "#" is the optional device id formatted as `/[0-9a-z]{16}/`, and the third argument "CPE" is the optional data center abbreviation.
+```
 wget https://raw.githubusercontent.com/blackfisk-tech/monitor-service/master/install.sh ~/.
 chmod a+x ~/install.sh
-~/install.sh "A" "#" "CPE"
+~/install.sh "PI" "#" "CPE"
+```
+
+### Debugging
+Uses debug package from TJ Holowaychuk (https://github.com/visionmedia/debug)
+Enable full debug by setting env variable DEBUG=*
+
+### Server Naming
+By default the server name is read from the OS hostname and must follow `/[\w]*-[\w]*-[\w]*/` format. Else the servername is read from /etc/servername.conf.
+
+### CUPS
+You can access the cups interface in a browser at `{hostname OR private ip}:631`. You can also use the cups cli. See https://github.com/apple/cups for more information.
--- index.js
+++ index.js
@@ -10,120 +10,47 @@ const cupsdm = require('cupsdm')
 const cups = require('ncups')
 const Printer = require('node-printer')
 const pkg = require('./package.json')
-
-const manager = cupsdm.createManger({ autoAddPrinters: false })
+const debug = require('debug')('monitor-service')
 
 let serverList = {}
 let printerList = {}
+// * Server Name
 let servername = os.hostname()
-let ipAddress = { public: { ip4: null, ip6: null }, private: { ip4: null, ip6: null } }
-
 if (servername.split('-').length !== 3) {
   servername = fs.readFileSync('/etc/servername.conf', 'utf8').trim()
 }
-const socket = io.connect(os.platform() === 'win32' ? 'http://localhost:3000' : 'https://ws.dev.blackfisk.com', { 
-  transports: ['websocket'],
-  query: 'servername=' + servername + '&version=' + pkg.version,
-})
-
-publicIp.v4().then(ip => {
-  ipAddress.public.ip4 = ip
-  console.log(`connect ${servername} - ${ipAddress.public.ip4}`)
-})
-
-_.each(os.networkInterfaces(), iface => {
-  _.each(iface, adapter => {
-    if (adapter.address.match(/(^192\.168\.)|(^10\.)|(^172\.1[6-9]\.)|(^172\.2[0-9]\.)|(^172\.3[0-1]\.)|(^::1$)|(^[fF][cCdD])/) && !adapter.internal) {
-      if (ipAddress.private.ip4 === null) {
-        ipAddress.private.ip4 = adapter.address
-      }
-    }
-  })
-})
-
-/*
-  Announce Yourself on the network to see if any other services are listening
- */
-bonjour.publish({ name: servername, type: 'blackfisk.server', port: 443 })
-
-/*
-  usbDetect.on('add', function (device) {
-  })
-
-  usbDetect.find(function (err, devices) {
-    console.log('find', devices, err)
-  })
-*/
-
-socket
-  .on('connect', function () {
-    // console.log(socket.id)
-    heartbeat()
-  })
-  .on('print', function (data) {
-    let thisPrinter = new Printer(data.printer)
-    let thisJob = thisPrinter.printText(data.document, data.options)
-    thisJob.on('sent', function () {
-      console.log('sent', thisJob)
-    })
-    thisJob.on('completed', function () {
-      console.log('Job ' + thisJob.identifier + 'has been printed')
-      thisJob.removeAllListeners()
-    })
-  })
-  .on('bash', function (data) {
-    console.log('executing command: ' + data.cmd)
-    exec(data.cmd, execErrorHandling)
-  })
-  .on('git', async function (data) {
-    if (data.repo === 'monitor-service') {
-      await exec('chmod a+x /home/blackfisk/monitor-service/upgrade.sh', execErrorHandling)
-      await sleep(2000)
-      await exec('/home/blackfisk/monitor-service/upgrade.sh', execErrorHandling)
-    }
-  })
-  .on('disconnect', function (reason) {
-    console.error('goodbye', reason)
-  })
-  .on('heartbeat', function (a, b, c) {
-    heartbeat()
-  })
-  .on('error', function (error) {
-    console.error('error', error)
-  })
-  .on('connect_error', function (error) {
-    console.error('connect_error', error)
-  })
-  .on('connect_timeout', function (timeout) {
-    console.error('connect_timeout', timeout)
-  })
-  .on('reconnecting', function (a, b, c) {
-    console.error('reconnecting', a, b, c)
-  })
-  .on('reconnect_error', function (a, b, c) {
-    console.error('reconnect_error', a, b, c)
-    process.exit(0)
-  })
-  .on('reconnect_failed', function (a, b, c) {
-    console.error('reconnect_failed', a, b, c)
-    process.exit(0)
-  })
-
-manager.start()
-
-function findOnlineServers () {
-  _.each(serverList, server => {
-    server.online = false
-  })
-
-  bonjour.find({ type: 'blackfisk.server' }, function (server) {
-    serverList[server.name] = server
-    serverList[server.name]['online'] = true
-  })
-  socket.emit('blackfisk', { command: 'serverList', servers: serverList })
+// * Socket Servers
+const sockets = {}
+const serverSocket = {}
+const socketServers = {
+  'https://ws.dev.blackfisk.com': {
+    query: `servername=${servername}&version=${pkg.version}`,
+    transports: ['websocket']
+  },
+  'https://ws.sandbox.blackfisk.com': {
+    query: `servername=${servername}&version=${pkg.version}`,
+    transports: ['websocket']
+  }
+  // {
+  //   uri: 'https://ws.app.blackfisk.com',
+  //   query: `servername=${servername}&version=${pkg.version}`,
+  //   transports: ['websocket']
+  // }
 }
+
+// * CUPS Manager
 let timeoutFindPrinterOnline = null
-function findOnlinePrinters () {
+// Note: yes, the library actually spells their method "createManger".
+const manager = cupsdm.createManger({ autoAddPrinters: false })
+const startCUPS = () => {
+  // Announce Yourself on the network to see if any other services are listening
+  bonjour.publish({ name: servername, type: 'blackfisk.server', port: 443 })
+  // Start Manager
+  manager.start()
+  // Listen for Printer Up, Down, and Add
+  registerCupsListeners()
+}
+const findOnlinePrinters = () => {
   _.each(printerList, printer => {
     printer.online = false
   })
@@ -136,14 +63,106 @@ function findOnlinePrinters () {
       }
     })
   })()
-
-  socket.emit('blackfisk', { command: 'printerList', printers: printerList })
+  _.each(sockets, socket => {
+    socket.emit('blackfisk', { command: 'printerList', printers: printerList })
+  })
 }
+const findOnlineServers = () => {
+  _.each(serverList, server => {
+    server.online = false
+  })
 
-function heartbeat () {
-  console.log('heartbeat')
+  bonjour.find({ type: 'blackfisk.server' }, function (server) {
+    serverList[server.name] = server
+    serverList[server.name]['online'] = true
+  })
+  _.each(sockets, socket => {
+    socket.emit('blackfisk', { command: 'serverList', servers: serverList })
+  })
+}
+const registerCupsListeners = async () => {
+  /*
+    Record Printers
+  */
+  manager.on('up', nodes => {
+    _.each(nodes, async node => {
+      if (node.model.indexOf('Zebra') !== -1) {
+        node.driver = {
+          id: '',
+          makeAndModel: 'Zebra ZPL Label Printer',
+          lang: 'en',
+          driver: 'drv:///sample.drv/zebra.ppd'
+        }
+        let serialNumber = _.last(node.uri.split('='))
+        node.printer.name = `Zebra_ZPL_Serial_${serialNumber}`
+      }
+      if (node.uri.indexOf('usb') !== -1) {
+        manager._addPrinters([node])
+        console.log('printer up', node.printer.name)
+        _.each(sockets, (socket) => {
+          socket.emit('printer', {
+            command: 'printer.up',
+            ...node
+          })
+        })
+      }
+      clearTimeout(timeoutFindPrinterOnline)
+      timeoutFindPrinterOnline = setTimeout(() => findOnlinePrinters(), 10 * 1000)
+    })
+  })
+
+  manager.on('down', nodes => {
+    _.each(nodes, async node => {
+      if (node.uri.indexOf('usb') !== -1) {
+        console.log('printer down', node.printer.name)
+        await cups.uninstall(node.printer.name)
+        _.each(sockets, (socket) => {
+          socket.emit('printer', {
+            command: 'printer.down',
+            ...node
+          })
+        })
+        clearTimeout(timeoutFindPrinterOnline)
+        timeoutFindPrinterOnline = setTimeout(() => findOnlinePrinters(), 10 * 1000)
+      }
+    })
+  })
+
+  manager.on('addPrinters', nodes => {
+    findOnlinePrinters()
+  })
+}
+// * Ip Addresses
+const ipAddress = { public: { ip4: null, ip6: null }, private: { ip4: null, ip6: null } }
+const detectIPAddress = () => {
+  publicIp.v4().then(ip => {
+    ipAddress.public.ip4 = ip
+    console.log(`connect ${servername} - ${ipAddress.public.ip4}`)
+  })
+  _.each(os.networkInterfaces(), iface => {
+    _.each(iface, adapter => {
+      if (adapter.address.match(/(^192\.168\.)|(^10\.)|(^172\.1[6-9]\.)|(^172\.2[0-9]\.)|(^172\.3[0-1]\.)|(^::1$)|(^[fF][cCdD])/) && !adapter.internal) {
+        if (ipAddress.private.ip4 === null) {
+          ipAddress.private.ip4 = adapter.address
+          return false
+        }
+      }
+    })
+    if (ipAddress.private.ip4 !== null) {
+      return false
+    }
+  })
+}
+// * Socket Servers
+const heartbeat = (socket) => {
   findOnlineServers()
   findOnlinePrinters()
+  const socketServer = _.invert(serverSocket)
+  console.log('heartbeat', {
+    socketServer: socketServer[socket.id],
+    servername: servername,
+    socketId: socket.id
+  })
   socket.emit('response', {
     command: 'heartbeat',
     clientID: socket.id,
@@ -159,9 +178,9 @@ function heartbeat () {
     ipAddress: ipAddress
   })
 }
-
-function execErrorHandling (error, stdout, stderr) {
-  console.log(error, stdout, stderr)
+const execErrorHandling = (error, stdout, stderr, socket) => {
+  console.log('ExecError', error, stdout)
+  console.error(stderr)
   socket.emit('response', {
     command: 'bash',
     error: error,
@@ -169,58 +188,81 @@ function execErrorHandling (error, stdout, stderr) {
     stderr: stderr
   })
 }
-
-/*
-  Record Printers
- */
-manager.on('up', nodes => {
-  _.each(nodes, async node => {
-    if (node.model.indexOf('Zebra') !== -1) {
-      node.driver = {
-        id: '',
-        makeAndModel: 'Zebra ZPL Label Printer',
-        lang: 'en',
-        driver: 'drv:///sample.drv/zebra.ppd'
-      }
-      let serialNumber = _.last(node.uri.split('='))
-      node.printer.name = `Zebra_ZPL_Serial_${serialNumber}`
-    }
-    if (node.uri.indexOf('usb') !== -1) {
-      manager._addPrinters([node])
-      console.log('printer up', node.printer.name)
-      socket.emit('printer', {
-        command: 'printer.up',
-        ...node
-      })
-    }
-
-    clearTimeout(timeoutFindPrinterOnline)
-    timeoutFindPrinterOnline = setTimeout(() => findOnlinePrinters(), 10 * 1000)
-  })
-})
-
-manager.on('down', nodes => {
-  _.each(nodes, async node => {
-    if (node.uri.indexOf('usb') !== -1) {
-      console.log('printer down', node.printer.name)
-      await cups.uninstall(node.printer.name)
-
-      socket.emit('printer', {
-        command: 'printer.down',
-        ...node
-      })
-      clearTimeout(timeoutFindPrinterOnline)
-      timeoutFindPrinterOnline = setTimeout(() => findOnlinePrinters(), 10 * 1000)
-    }
-  })
-})
-
-manager.on('addPrinters', nodes => {
-  findOnlinePrinters()
-})
-
-function sleep (ms) {
+const sleep = (ms) => {
   return new Promise(resolve => {
     setTimeout(resolve, ms)
   })
 }
+const registerSocketListeners = async (socket, conf, uri) => {
+  socket
+    .on('connect', function () {
+      console.log(`Connected to ${uri} with socketId:`, socket.id)
+      serverSocket[uri] = socket.id
+      sockets[socket.id] = socket
+      heartbeat(socket)
+    })
+    .on('print', function (data) {
+      let thisPrinter = new Printer(data.printer)
+      let thisJob = thisPrinter.printText(data.document, data.options)
+      thisJob.on('sent', function () {
+        debug('sent', thisJob)
+      })
+      thisJob.on('completed', function () {
+        console.log('Job ' + thisJob.identifier + 'has been printed')
+        thisJob.removeAllListeners()
+      })
+    })
+    .on('bash', function (data) {
+      console.log('Executing command: ' + data.cmd)
+      exec(data.cmd, execErrorHandling, socket)
+    })
+    .on('git', async function (data) {
+      if (data.repo === 'monitor-service') {
+        await exec('chmod a+x /home/blackfisk/monitor-service/upgrade.sh', execErrorHandling)
+        await sleep(2000)
+        await exec('/home/blackfisk/monitor-service/upgrade.sh', execErrorHandling)
+      }
+    })
+    .on('disconnect', function (reason) {
+      console.error('goodbye', reason)
+    })
+    .on('heartbeat', function (a, b, c) {
+      heartbeat(socket)
+    })
+    .on('error', function (error) {
+      console.error('error', error)
+    })
+    .on('connect_error', function (error) {
+      console.error('connect_error', error)
+    })
+    .on('connect_timeout', function (timeout) {
+      console.error('connect_timeout', timeout)
+    })
+    .on('reconnecting', function (a, b, c) {
+      console.error('reconnecting', a, b, c)
+    })
+    .on('reconnect_error', function (a, b, c) {
+      console.error('reconnect_error', a, b, c)
+      process.exit(0)
+    })
+    .on('reconnect_failed', function (a, b, c) {
+      console.error('reconnect_failed', a, b, c)
+      process.exit(0)
+    })
+}
+const startSockets = () => {
+  _.each(socketServers, (conf, uri) => {
+    const socket = io(uri, {
+      transports: conf.transports,
+      query: conf.query
+    })
+    console.log('Attempting socket connection:', {uri: uri, query: socket.query})
+    registerSocketListeners(socket, conf, uri)
+  })
+}
+// * Startup
+detectIPAddress()
+// CUPS Driver
+startCUPS()
+// Sockets
+startSockets()
